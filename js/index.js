befe7299a17c3b1d98c803698e658399d9c60347
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-01-20 18:56:22 +0530

    feat: func getComputerChoice returns rock/paper/scissors

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 function getComputerChoice () {
     let choice = Math.floor(Math.random()*3) + 1;
+    let ret;
+    switch (choice) {
+        case 1:
+            ret = "Rock"
+            break;
+        case 2:
+            ret = "Paper"
+            break;
+        case 3:
+            ret = "Scissors"
+            break;
+    }
+    return ret;
 }
