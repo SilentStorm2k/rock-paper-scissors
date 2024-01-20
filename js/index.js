6a66749b27de57de0f1091efcd78a433aed1d702
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-01-20 19:59:52 +0530

    feat: simulates round of rock paper scissors
    if both comp and player choose same, replays round

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const { error, warn } = require("console");
+
 function getComputerChoice () {
     let choice = Math.floor(Math.random()*3) + 1;
     let ret;
@@ -17,15 +19,39 @@ function getComputerChoice () {
 
 function playRound (playerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    let computerSelection = getComputerChoice();
-    if (computerSelection == playerSelection) {}
-        // replay round
-    let playerChoice
-    switch (playerSelection) {
-        case "rock":        playerChoice = 1; break;
-        case "paper":       playerChoice = 2; break;
-        case "scissors":    playerChoice = 3; break;
-        default:            playerChoice = 0;
+    const validChoices = ["rock", "paper", "scissors"];
+    if (!validChoices.include(playerSelection)) {
+        console.warn(`${playerSelection} is not a valid option`)
+        return
     }
-    
+    let result = simulatePlay(playerSelection, getComputerChoice());
+    while (result == "replay round") {
+        console.log("TIE: replay round")
+        playerSelection = prompt()
+        result = simulatePlay(playerSelection, getComputerChoice());
+    }
+    return result;
+}
+
+function simulatePlay (player, computer) {
+    if (player == computer) {
+        return "replay round"
+    }
+    switch (player) {
+        case "rock":   
+            if (computer == "paper") 
+                result = "You Lose!" + computer + " beats " + player 
+            break;
+        case "paper": 
+            if (computer == "scissors") 
+                result = "You Lose!" + computer + " beats " + player
+            break;
+        case "scissors":  
+            if (computer == "rock") 
+                result = "You Lose!" + computer + " beats " + player
+            break;
+        default:   
+            result = "You Win!" + player + " beats " + computer
+    }
+    return result;
 }
