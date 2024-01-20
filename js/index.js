70cfba75cd4cbddfd47ed6aa39c63624b64a2f16
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-01-20 20:17:05 +0530

    refactor: getComputerChoice is easier to read

--- js/index.js
+++ js/index.js
@@ -2,15 +2,9 @@ function getComputerChoice () {
     let choice = Math.floor(Math.random()*3) + 1;
     let ret;
     switch (choice) {
-        case 1:
-            ret = "rock"
-            break;
-        case 2:
-            ret = "paper"
-            break;
-        case 3:
-            ret = "scissors"
-            break;
+        case 1: ret = "rock";       break;
+        case 2: ret = "paper";      break;
+        case 3: ret = "scissors";   break;
     }
     return ret;
 }
