b98eeff7b24fef0c9215f52d6fccc511b8676b78
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-01-20 19:03:42 +0530

    feat: getComputerChoice returns random int 1-3 representing 3 choices

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 function getComputerChoice () {
-    let choice = Math.floor(Math.random()*3) + 1;
-    let ret;
-    switch (choice) {
-        case 1:
-            ret = "Rock"
-            break;
-        case 2:
-            ret = "Paper"
-            break;
-        case 3:
-            ret = "Scissors"
-            break;
+    return Math.floor(Math.random()*3) + 1;
+}
+
+function playRound (playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let computerSelection = getComputerChoice();
+    if (computerSelection == playerSelection) {}
+        // replay round
+    let playerChoice
+    switch (playerSelection) {
+        case "rock":        playerChoice = 1; break;
+        case "paper":       playerChoice = 2; break;
+        case "scissors":    playerChoice = 3; break;
+        default:            playerChoice = 0;
     }
-    return ret;
+
 }
