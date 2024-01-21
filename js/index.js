6fa9dc609ce5674b0f3eedf97fed3da1c4f0000e
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-01-22 00:22:55 +0530

    feat: added endgame function, refactor better

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
+const userChoice = document.querySelector(".user-choice-container");
+let currentUserChoice = rock;
+let playerWins = 0, computerWins = 0, roundCount = 1;
+
+userChoice.addEventListener('click', startRound)
+
+function startRound(event) {
+    if (event.target.nodeName !== "BUTTON")
+            return;
+    currentUserChoice = event.target.id;
+    playRound(currentUserChoice);
+}
+
+function playRound (playerSelection) {
+    let computerChoice = getComputerChoice();
+    let result = simulatePlay(playerSelection, computerChoice);
+    displayComputerChoice(computerChoice);
+    if (result.substring(0, 7) == "You Win")
+            playerWins++;
+    if (result.substring(0, 7) == "You Los")
+            computerWins++;
+    if (playerWins >= 1 || computerWins >= 5) {
+        let winner = playerWins >= 1 ? "You win the game" : "You lose! Computer wins!";
+        endGame(winner);
+    }
+    return result;
+}
+
 function getComputerChoice () {
     let choice = Math.floor(Math.random()*3) + 1;
     let ret;
@@ -7,6 +35,20 @@ function getComputerChoice () {
         case 3: ret = "scissors";   break;
     }
     return ret;
+}
+
+function simulatePlay (player, computer) {
+    if (player == computer) 
+        return "TIE"
+    else if (computer == "paper" && player == "rock") 
+        result = "You Lose! " + computer + " beats " + player 
+    else if (computer == "scissors" && player == "paper") 
+        result = "You Lose! " + computer + " beats " + player
+    else if (computer == "rock" && player == "scissors") 
+        result = "You Lose! " + computer + " beats " + player
+    else
+        result = "You Win! the" + player + " beats " + computer
+    return result;
 }
 
 const computerChoiceContainer = document.querySelector(".computer-choice");
@@ -26,56 +68,7 @@ function clearComputerChoice() {
         computerChoiceContainer.removeChild(computerChoiceContainer.lastChild);
 }
 
-let playerWins = 0, computerWins = 0, roundCount = 1;
-
-function playRound (playerSelection) {
-    let computerChoice = getComputerChoice();
-    let result = simulatePlay(playerSelection, computerChoice);
-    displayComputerChoice(computerChoice);
-    if (result.substring(0, 7) == "You Win")
-            playerWins++;
-    if (result.substring(0, 7) == "You Los")
-            computerWins++;
-    if (playerWins >= 5 || computerWins >= 5) {
-        let winner = playerWins >= 5 ? "You win the game" : "You lose! Computer wins!";
-        endGame(winner);
-    }
-    return result;
-}
-
-function startRound(event) {
-    if (event.target.nodeName !== "BUTTON")
-            return;
-    currentUserChoice = event.target.id;
-    playRound(currentUserChoice);
-}
-
 function endGame(winner) {
     userChoice.removeEventListener('click', startRound);
-    console.log("GAME ENDED")
+    console.log(winner)
 }
-
-function simulatePlay (player, computer) {
-    if (player == computer) 
-        return "TIE"
-    else if (computer == "paper" && player == "rock") 
-        result = "You Lose! " + computer + " beats " + player 
-    else if (computer == "scissors" && player == "paper") 
-        result = "You Lose! " + computer + " beats " + player
-    else if (computer == "rock" && player == "scissors") 
-        result = "You Lose! " + computer + " beats " + player
-    else
-        result = "You Win! the" + player + " beats " + computer
-    return result;
-}
-
-const userChoice = document.querySelector(".user-choice-container");
-let currentUserChoice = rock;
-
-userChoice.addEventListener('click', (event) => {
-    if (event.target.nodeName !== "BUTTON")
-        return;
-    currentUserChoice = event.target.id;
-    playRound(currentUserChoice);
-})
-
